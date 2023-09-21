04724914ba841698567108222ccc79c2fd942b19
Mahesh Misal <maheshmisal2018@gmail.com>
2023-09-21 18:11:52 +0530

    comleted

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -3,25 +3,20 @@ package com.driver;
 public class Boat implements WaterVehicle{
     private String name;
     private int capacity;
-
     public Boat(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
     }
-
     public void setName(String name) {
         this.name = name;
     }
     public void setCapacity(int capacity) {
         this.capacity = capacity;
     }
-
-    public String getVehicleName()
-    {
+    public String getVehicleName() {
         return name;
     }
-    public int getVehicleCapacity()
-    {
+    public int getVehicleCapacity() {
         return capacity;
     }
 
--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -2,6 +2,4 @@ package com.driver;
 
 public interface WaterVehicle {
 
-
-
 }
